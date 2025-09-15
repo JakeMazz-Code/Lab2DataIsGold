27d259b87ddcbcb7545adbd41eb11ae516b6a523
JakeMazz-Code <jake@jmazzarella.com>
2025-09-15 19:00:56 -0400

    remove demo/test artifacts and add strict .gitignore

--- tmp_invalids.py
+++
@@ -1,9 +0,0 @@
-import json
-D=json.load(open('data/sample_output.json','r',encoding='utf-8'))
-miss=[]
-for r in D:
-    ok = bool(r.get('course_code')) and bool(r.get('section')) and bool(r.get('subject'))
-    if not ok:
-        miss.append((r.get('subject'), r.get('number'), r.get('section'), r.get('course_code')))
-print('invalid_count', len(miss))
-print(miss[:10])
